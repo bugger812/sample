0be85b1fa014aeae5aa640e1c337efebcd2b28b0
PavelVlasenko <vlasenko-pav@yandex.ru>
2017-03-25 13:44:38 +0700

    remove logback

--- src/main/java/com/example/Aspects/LoggingAspect.java
+++ src/main/java/com/example/Aspects/LoggingAspect.java
@@ -4,22 +4,18 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
-
     @Pointcut("execution(public * *(..))")
     private void anyPublicMethod() {}
 
     @Before("anyPublicMethod() && @annotation(org.springframework.web.bind.annotation.RequestMapping)")
     public void logAction(JoinPoint joinPoint) {
-        LOGGER.info("Execute method " + joinPoint.getSignature().getName());
+        System.out.println("Execute method " + joinPoint.getSignature().getName());
     }
 
 }
