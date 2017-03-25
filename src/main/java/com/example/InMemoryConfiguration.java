546c9d1dd6fa3898a84f544bd23dd0e4b8815c8e
PavelVlasenko <vlasenko-pav@yandex.ru>
2017-03-26 00:24:12 +0700

    database hook

--- src/main/java/com/example/InMemoryConfiguration.java
+++ src/main/java/com/example/InMemoryConfiguration.java
@@ -14,6 +14,7 @@ import java.util.Properties;
 @Configuration
 public class InMemoryConfiguration {
 
+
     @Bean
     public DataSource dataSource() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
