f2db0ba9935823a43d6d52407b38673c2a88164a
Sason <sason@gmail.com>
2017-05-22 21:24:00 +0300

    user profile

--- src/main/java/com/example/Objects/UserObject.java
+++ src/main/java/com/example/Objects/UserObject.java
@@ -8,27 +8,4 @@ public class UserObject {
     private String address;
     private String phone;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
